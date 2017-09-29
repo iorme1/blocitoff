7a7edd272f71f2267e3e5699300f1a12aa41aa74
Isaac Orme <isorme1@gmail.com>
2017-09-29 12:57:35 -0700

    mark-todo-items-as-complete: completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    if current_user.items.count < 1
+      flash[:notice] = "You have completed all of your To-do items. Congratulations!"
+      redirect_to root_path
+    elsif current_user.items.count >= 1
+      flash[:notice] = "Nice work, #{current_user.items.count} more to go!"
+      redirect_to root_path
+    else
+      flash[:alert] = "To-do item was not deleted. Please try again."
+      redirect_to root_path
+    end
+  end
+
   private
 
   def items_params
