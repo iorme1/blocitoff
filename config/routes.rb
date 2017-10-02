ab0c8d166802757c11c1dbea767fdad88ce5f77e
Isaac Orme <isorme1@gmail.com>
2017-10-01 18:46:32 -0700

    alert-message-sign-up fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
 
   resources :users, only: [:show] do
       resources :items, only: [:create, :destroy]
