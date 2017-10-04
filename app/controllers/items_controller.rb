a60aacb896c2bc04dddd94a6b4125f7b891798ef
Charlie Gaines <beaugaines@gmail.com>
2017-10-04 15:43:11 -0400

    ajaxify item form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = current_user.items.new(items_params)
-
-    if @item.save
-      redirect_to root_path
-      flash[:notice] = "To-do item successfully saved!"
-    else
-      flash[:alert] = "To-do item was not saved. Please try again."
-      redirect_to root_path
-    end
+    @item.save
   end
 
   def complete
