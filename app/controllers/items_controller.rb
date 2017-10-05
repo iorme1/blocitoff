2d633a44049099d6e6bf0d89b59d452e48aeb842
Isaac Orme <isorme1@gmail.com>
2017-10-04 17:45:56 -0700

    bootswatch-ajax-update: completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,32 @@
 class ItemsController < ApplicationController
 
-
   def create
     @item = current_user.items.new(items_params)
+    @item.save
+  end
 
-    if @item.save
-      redirect_to root_path
-      flash[:notice] = "To-do item successfully saved!"
+  def complete
+    @user = User.find(params[:user_id])
+    @item = @user.items.find(params[:id])
+    if @item.completed_at
+      @item.update(completed_at: nil)
     else
-      flash[:alert] = "To-do item was not saved. Please try again."
-      redirect_to root_path
+      @item.touch(:completed_at)
     end
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
+    @items = current_user.items.where.not(completed_at: nil)
+    @items.destroy_all
     if current_user.items.count < 1
       flash[:notice] = "You have completed all of your To-do items. Congratulations!"
-      redirect_to root_path
+      redirect_to user_path
     elsif current_user.items.count >= 1
       flash[:notice] = "Nice work, #{current_user.items.count} more to go!"
-      redirect_to root_path
+      redirect_to user_path
     else
       flash[:alert] = "To-do item was not deleted. Please try again."
-      redirect_to root_path
+      redirect_to user_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'items/create'
-
-  get 'users/show'
-
   devise_for :users, :controllers => { :registrations => "registrations" }
 
   resources :users, only: [:show] do
       resources :items, only: [:create, :destroy]
+      patch 'completed/:id', to: 'items#complete', as: :complete_item
     end
-
-  get 'welcome/index'
-  get 'welcome/show'
 
   root 'users#show'
 
